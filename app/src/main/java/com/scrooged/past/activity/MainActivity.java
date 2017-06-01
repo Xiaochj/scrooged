451643b49bc2fa36714c763eb26f7f37b799a188
肖长剑 <13772521682@yeah.net>
2017-06-01 15:00:24 +0800

    test

---
+++ app/src/main/java/com/scrooged/past/activity/MainActivity.java
@@ -0,0 +1,10 @@
+package com.scrooged.past.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by xiaochj on 2017/6/1.
+ */
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/com/scrooged/past/utils/DatetimeUtil.java
@@ -0,0 +1,10 @@
+package com.scrooged.past.utils;
+
+/**
+ * 获取系统日期时间的工具类
+ * Created by xiaochj on 2017/6/1.
+ */
+
+public class DatetimeUtil {
+
+}
